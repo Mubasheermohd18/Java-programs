bc39c33c91b08dedbb54a2ebaf24f0b249aef332
Mohammed Mubasheeruddin <b24ds104@kitsw.ac.in>
2026-06-18 15:19:48 +0530

    Add files via upload

---
+++ divisor3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class divisor3 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        if((a+b)/2%2==0){
+            System.out.println((a+b)/2);
+        }
+        else{
+            System.out.println("No");
+        }
+        sc.close();
+    }
+}
